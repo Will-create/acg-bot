84cae07fe0e0d25680e34e08cea206bc40a15c29
Louis Bertson <louisbertson@gmail.com>
2020-10-27 12:01:00 +0000

    intégration terminée

---
+++ resources/views/layouts/footer-scripts.blade.php
@@ -0,0 +1,37 @@
+		<!-- BACK-TO-TOP -->
+		<a href="#top" id="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+		<!-- JQUERY JS -->
+		<script src="{{URL::asset('assets/js/jquery-3.4.1.min.js')}}"></script>
+
+		<!-- BOOTSTRAP JS -->
+		<script src="{{URL::asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+		<script src="{{URL::asset('assets/plugins/bootstrap/js/popper.min.js')}}"></script>
+
+		<!-- SPARKLINE JS-->
+		<script src="{{URL::asset('assets/js/jquery.sparkline.min.js')}}"></script>
+
+		<!-- CHART-CIRCLE JS-->
+		<script src="{{URL::asset('assets/js/circle-progress.min.js')}}"></script>
+
+		<!-- RATING STARJS -->
+		<script src="{{URL::asset('assets/plugins/rating/jquery.rating-stars.js')}}"></script>
+
+		<!-- EVA-ICONS JS -->
+		<script src="{{URL::asset('assets/iconfonts/eva.min.js')}}"></script>
+		
+		<!--HORIZONTAL JS-->
+		<script src="{{URL::asset('assets/plugins/horizontal-menu/horizontal-menu.js')}}"></script>
+
+		<!-- CUSTOM SCROLLBAR JS-->
+		<script src="{{URL::asset('assets/plugins/scroll-bar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+
+		<!-- STICKY JS -->
+		<script src="{{URL::asset('assets/js/stiky.js')}}"></script>
+
+		@yield('js')
+		<!-- SIDEBAR JS -->
+		<script src="{{URL::asset('assets/plugins/sidebar/sidebar.js')}}"></script>
+		
+		<!-- CUSTOM JS -->
+		<script src="{{URL::asset('assets/js/custom.js')}}"></script>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Systeme de paiement en masse">
+	<meta name="author" content="Switch Maker">
+	<meta name="keywords" content="">
+	<title> {{ $titrePage ??  'Systeme de paiement en masse'}} </title>
+    @include('layouts.head')
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+</head>
+	<body class="app sidebar-mini">
+		<div id="global-loader">
+			<img src="{{URL::asset('assets/images/loader.svg')}}" class="loader-img" alt="Loader">
+		</div>
+		<div class="page">
+			<div class="page-main">
+			
+				
+				@include('layouts.aside-menu')
+			
+				@include('layouts.head')
+				<br>
+				<div class="app-content">
+					<div class="side-app">
+						@yield('page-header')
+						@yield('content')
+					</div>
+				</div>
+			</div>
+			@include('layouts.aside-bar')
+			@include('layouts.footer')
+		</div>
+		@include('layouts.footer-scripts')
+	</body>
+</html>
