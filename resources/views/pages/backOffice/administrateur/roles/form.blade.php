24cf8d02629c1238a9240b8f61976b35b5b18270
Louis Bertson <louisbertson@gmail.com>
2020-11-11 11:07:17 +0000

    finition

--- resources/views/pages/backOffice/administrateur/roles/form.blade.php
+++ resources/views/pages/backOffice/administrateur/roles/form.blade.php
@@ -3,14 +3,11 @@
 @section('css')
         <!-- INTERNAL SELECT2 CSS -->
 		<link href="{{URL::asset('assets/plugins/fileuploads/css/fileupload.css')}}" rel="stylesheet" type="text/css" />
-
 		<link href="{{URL::asset('assets/plugins/select2/select2.min.css')}}" rel="stylesheet" />
-
 		<!-- INTERNAL  DATA TABLE CSS-->
 		<link href="{{URL::asset('assets/plugins/datatable/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
 		<link href="{{URL::asset('assets/plugins/datatable/responsivebootstrap4.min.css')}}" rel="stylesheet" />
         <link href="{{URL::asset('assets/plugins/datatable/fileexport/buttons.bootstrap4.min.css')}}" rel="stylesheet" />
-
           <!-- INTERNAL PRISM CSS -->
           <link href="{{URL::asset('assets/plugins/prism/prism.css')}}" rel="stylesheet">
           	<!-- INTERNAL TELEPHONE CSS-->
@@ -33,7 +30,6 @@
                         </span>
                         Tous les Rôles</a>
                     </button>
-
 					</div>
 				</div>
 				<!-- PAGE-HEADER END -->
@@ -68,11 +64,6 @@
                             @enderror
                     </div>
                 </div>
-                
-                    
-                
-                
-                
             </div>
         </div>
     </div>
@@ -80,7 +71,6 @@
         <button type="submit" class="btn btn-primary"> <span>
             <i class="fe fe-save"></i>
         </span> Enregistrer</button>
-
     </div>
 </form>
 @stop
@@ -88,7 +78,6 @@
 <script src="{{URL::asset('assets/plugins/fileuploads/js/fileupload.js')}}"></script>
 <script src="{{URL::asset('assets/plugins/fileuploads/js/file-upload.js')}}"></script>
 <script src="{{URL::asset('assets/plugins/datatable/jquery.dataTables.min.js')}}"></script>
-
     <!-- INTERNALPRISM JS -->
     <script src="{{URL::asset('assets/plugins/prism/prism.js')}}"></script>
         <!-- INTERNAL TELEPHONE JS -->
