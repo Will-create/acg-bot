42044f056f99efbe938da99c19c480d0b55e4cd3
unknown <ouedharoun3@gmail.com>
2020-12-07 16:04:30 +0000

    create admin acount in seed

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -24,7 +24,20 @@ class UserTableSeeder extends Seeder
         ];
         //User::truncate();
         $roles = Role::all();
-
+        User::create([
+            'nom'                   => $faker->firstName,
+            'titre'                 => $faker->title,
+            'actif'                 => true,
+            'role_id'               => 1,
+            'profile_photo_path'    => "/images/pngs/bg-l.png",
+            'prenom'                => $faker->lastName,
+            'email'                 => 'admin@uicn.com',
+            'tel'                   => $faker->phoneNumber,
+            'password'              => Hash::make('00000000'),
+            'localite_id'              => $faker->numberBetween($min = 1, $max = 5),
+            'uuid'                  => Str::uuid(),
+            'pay_id'                => rand(1,16)
+        ]);
         foreach ($roles as $key => $role) {
             User::create([
                 'nom'                   => $faker->firstName,
