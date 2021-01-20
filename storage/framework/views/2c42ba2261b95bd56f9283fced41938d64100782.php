9a0c8c6cde0ab7e1e1b2fbefb355bf3d63e809ed
unknown <h.ouedraogo@switch-maker.com>
2021-01-20 10:41:50 +0100

    suppression cache

--- storage/framework/views/2c42ba2261b95bd56f9283fced41938d64100782.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH D:\switch_maker\war_crimes\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
